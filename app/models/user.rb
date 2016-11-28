fe001ef421a4258fc513e162300fbb1815166c91
deanfoulds <deanfoulds@gmail.com>
2016-11-28 13:25:12 +0000

    Finalised Katathon App

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, #:confirmable, (email confirmation not required at the moment)
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'visitors#home'
+  get '/gallery', to: 'pages#gallery', as: :gallery
+  get '/about', to: 'pages#about', as: :about
   devise_for :users
+  resources :users
 end
